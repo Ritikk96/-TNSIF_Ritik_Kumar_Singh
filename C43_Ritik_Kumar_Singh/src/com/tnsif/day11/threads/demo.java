05337fcea8a4b8c12bfa5eb693542c9ce2de93c3
RITIK SINGH <142474954+Ritikk96@users.noreply.github.com>
2024-09-02 23:57:07 +0530

    added
    day11

--- C43_Ritik_Kumar_Singh/src/com/tnsif/day11/threads/demo.java
+++ C43_Ritik_Kumar_Singh/src/com/tnsif/day11/threads/demo.java
@@ -1,5 +1,6 @@
-public class demo {
+package com.tnsif.day11.threads;
 
+public class demo {
 	public static void main(String[] args) {
 		System.out.println("the program continues....");
 		int data=100/0;
